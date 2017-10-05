b08e13fbc53ed6a095cf2d3e07eed748bc89d510
erdivartanovich <erdivartanovich@gmail.com>
2017-10-06 02:05:02 +0700

    add method handler

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
+// initialize our server class
 var jetx = function() {
     this.server = http.createServer((req, res) => {
         var url = req.url;
+        //filter url from our router list
         var route = this.router.filter(route => (route.url === url && route.method === req.method));
         if(route && route.length>0) {
             console.log("incoming request to url: ", route[0].url);
@@ -19,21 +21,25 @@ jetx.prototype.router = [];
 
 jetx.prototype.page404 = '<h1>404</h1><h4>Resource not accessible, stay away!</h4>';
 
-jetx.prototype.route = function(url, callback, method='GET') {
-   this.router.push({url, callback, method});
+jetx.prototype.route = function(url, method='GET', callback) {
+    this.router.push({url, method, callback});
 }
 
 jetx.prototype.get = function(url, callback) {
-    this.route(url, callback);
+    this.route(url, 'GET', callback);
 }
 
-jetx.prototype.response = function(res) {
-    return function(content, status=200, contentType='text/html') {
+jetx.prototype.post = function(url, callback) {
+    this.route(url, 'POST', callback);
+}
+
+jetx.prototype.response = (res) => 
+    (content, status=200, contentType='text/html') => {
         res.writeHead(status, {'Content-Type': contentType});
         res.write(content);
         res.end();
     }
-}
+
 
 jetx.prototype.listen =  function(port) {
     this.server.listen(port);
@@ -41,12 +47,16 @@ jetx.prototype.listen =  function(port) {
 }
 
 const app = new jetx();
-app.route('/', function(req, res) {
+app.route('/', 'GET', function(req, res) {
     res('hello world');
 });
 
-app.route('/test',  function(req, res) {
-    res('hello');
+app.get('/test',  function(req, res) {
+    res('this is the test response with get method');
+});
+
+app.post('/',  function(req, res) {
+    res('this is the post response');
 });
 
 app.listen(8080);
